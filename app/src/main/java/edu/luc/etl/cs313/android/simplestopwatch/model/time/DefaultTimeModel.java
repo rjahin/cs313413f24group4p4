8ec8e5fce3af59f7d18e266be93e964657a9d797
yared abebe leake <yaredaleake@gmail.com>
2024-12-05 00:21:43 -0600

    automatically starts to decrement when it hits 15,

--- app/src/main/java/edu/luc/etl/cs313/android/simplestopwatch/model/time/DefaultTimeModel.java
+++ app/src/main/java/edu/luc/etl/cs313/android/simplestopwatch/model/time/DefaultTimeModel.java
@@ -1,5 +1,8 @@
 package edu.luc.etl.cs313.android.simplestopwatch.model.time;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 import static edu.luc.etl.cs313.android.simplestopwatch.common.Constants.*;
 
 /**
@@ -8,6 +11,9 @@ import static edu.luc.etl.cs313.android.simplestopwatch.common.Constants.*;
 public class DefaultTimeModel implements TimeModel {
 
     private int runningTime = 0;
+    private boolean isDecrementing = false; // Flag to track if we're in decrement mode
+    private Timer decrementTimer;
+
 
     @Override
     public void resetRuntime() {
@@ -16,18 +22,41 @@ public class DefaultTimeModel implements TimeModel {
 
     @Override
     public void incRuntime() {
-        //runningTime = (runningTime + SEC_PER_TICK) % SEC_PER_HOUR;
-        if (runningTime < 99) {
+        if (!isDecrementing && runningTime < 99) {
             runningTime++; // Increment time up to 99
+
+            if (runningTime == 15) {
+                decRuntime(); // Switch to decrement mode when reaching 99
+            }
         }
     }
 
     @Override
-    public void decRuntime() { //implementation for decrementing runtime
-        //runningTime = (runningTime + SEC_PER_TICK) % SEC_PER_HOUR;
-        if (runningTime > 0) {
-            runningTime--; // Increment time up to 99
+    public void decRuntime() {
+        if (!isDecrementing) {
+            isDecrementing = true;
+
+            decrementTimer = new Timer();
+            decrementTimer.schedule(new TimerTask() {
+                @Override
+                public void run() {
+                    if (runningTime > 0) {
+                        runningTime--; // Decrement time down to 0
+                    } else {
+                        stopDecrementing(); // Stop when time reaches 0
+                    }
+                }
+            }, 1000, 1000); // Decrement time every 1000ms
         }
+    }
+
+    private void stopDecrementing() {
+        isDecrementing = false;
+        if (decrementTimer != null) {
+            decrementTimer.cancel(); // Stop the decrement timer
+            decrementTimer = null;
+        }
+        runningTime = 0; // Ensure time stays at 0
     }
 
     @Override
