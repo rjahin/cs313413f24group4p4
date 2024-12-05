8581bc70cc2147ab660a109c8065ed0839c10e9a
cogrodny <119828759+cogrodny@users.noreply.github.com>
2024-12-05 16:59:35 -0600

    fixed decrementing so it works properly now and adjusted so AlarmSounding doesnt send to StoppedState

--- app/src/main/java/edu/luc/etl/cs313/android/simplestopwatch/model/time/DefaultTimeModel.java
+++ app/src/main/java/edu/luc/etl/cs313/android/simplestopwatch/model/time/DefaultTimeModel.java
@@ -1,8 +1,5 @@
 package edu.luc.etl.cs313.android.simplestopwatch.model.time;
 
-import java.util.Timer;
-import java.util.TimerTask;
-
 import static edu.luc.etl.cs313.android.simplestopwatch.common.Constants.*;
 
 /**
@@ -11,8 +8,6 @@ import static edu.luc.etl.cs313.android.simplestopwatch.common.Constants.*;
 public class DefaultTimeModel implements TimeModel {
 
     private int runningTime = 0;
-    private boolean isDecrementing = false; // Flag to track if we're in decrement mode
-    private Timer decrementTimer;
 
 
     @Override
@@ -22,41 +17,16 @@ public class DefaultTimeModel implements TimeModel {
 
     @Override
     public void incRuntime() {
-        if (!isDecrementing && runningTime < 99) {
+        if (runningTime < 99) {
             runningTime++; // Increment time up to 99
-
-            if (runningTime == 15) {
-                decRuntime(); // Switch to decrement mode when reaching 99
-            }
         }
     }
 
     @Override
     public void decRuntime() {
-        if (!isDecrementing) {
-            isDecrementing = true;
-
-            decrementTimer = new Timer();
-            decrementTimer.schedule(new TimerTask() {
-                @Override
-                public void run() {
-                    if (runningTime > 0) {
-                        runningTime--; // Decrement time down to 0
-                    } else {
-                        stopDecrementing(); // Stop when time reaches 0
-                    }
-                }
-            }, 1000, 1000); // Decrement time every 1000ms
+        if (runningTime > 0) {
+            runningTime--;
         }
-    }
-
-    private void stopDecrementing() {
-        isDecrementing = false;
-        if (decrementTimer != null) {
-            decrementTimer.cancel(); // Stop the decrement timer
-            decrementTimer = null;
-        }
-        runningTime = 0; // Ensure time stays at 0
     }
 
     @Override
